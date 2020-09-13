b6d2a89eeb422bd3f381fdf9f9cd9da8eda51d6b
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-13 10:16:24 +0300

    create a Redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import { createStore } from "redux";
+
+const initialState = {};
+
+const reducer = (state = initialState, action) => {
+  console.log("reducer runs with", state, action);
+  return state;
+};
+
+const store = createStore(reducer);
+
 const App = () => {
   return <div className="container">Hello World!</div>;
 };
